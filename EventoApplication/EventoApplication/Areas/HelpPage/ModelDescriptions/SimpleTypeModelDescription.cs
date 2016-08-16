7154e31dc3192fd602909c18ed09bfe1807ea1b9
Dzenana <dkapetanov1@etf.unsa.ba>
2016-08-16 17:11:23 +0200

    Solution

---
+++ EventoApplication/EventoApplication/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EventoApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ EventoApplication/EventoApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EventoApplication.MvcApplication" Language="C#" %>
